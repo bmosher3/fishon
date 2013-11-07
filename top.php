0cb29516d17c321e6a44f5a1fccfd3b579b5804a
bmosher3 <bmosher3@gmail.com>
2013-11-07 11:39:51 -0500

    changed maptest to fishmap, added top.php

---
+++ top.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Fish On</title>
+<meta charset="utf-8">
+<meta name="author" content="Ben Mosher">
+<meta name="description" content="User submitted fishing reports for Rhode Island.">
+
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+	
+<link rel="stylesheet"
+href="style.css"
+type="text/css"
+media="screen">
+
+<script type="text/javascript">
+<!--
+function newWindow(imgName, winName, intWidth, intHeight){
+	var windowSize = 'width=' + intWidth + ',height=' + intHeight;
+	window.open(imgName, winName, windowSize)
+}
+//-->
+</script>
+	
+</head>
+<body><header>
+	<h1><b>Fish On</b></h1>
+</header>
+<nav>
+     <ol>
+<li><a href="home.php">Home</a></li>
+        <li><a href="checklist.php">Fish Map</a></li>	
+        <li><a href="status.php">Submit Report</a></li>	
+     </ol>
+</nav>
