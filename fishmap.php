b1b8d63c84e5a4e7e0b2d3082b06e40cca2f619c
bmosher3 <bmosher3@gmail.com>
2013-11-08 14:14:41 -0500

    started making form

--- fishmap.php
+++ fishmap.php
@@ -1,10 +1,12 @@
-
-
 <!DOCTYPE html>
 <html>
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
+    <link rel="stylesheet"
+          href="style.css"
+          type="text/css"
+          media="screen">
     <title>Info windows</title>
     <style>
       html, body, #map-canvas {
@@ -14,6 +16,14 @@
       }
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <script>
+        $(function() {
+            $( "#datepicker" ).datepicker();
+        });
+    </script>
     <script>
 
 function initialize() {
@@ -27,15 +37,15 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   var contentString = '<div id="report">'+
-      '<h1 id="firstHeading" class="firstHeading">Striped Bass</h1>'+
+      '<h1 id="firstHeading" class="firstHeading">Add Report</h1>'+
       '<div id="bodyContent">'+
-      '<p><b>10/23/13</b></p>'+
+      '<p><b>Date:   </b><input type="text" id="datepicker" /></p>'+
       '<p><b>5:00 PM</b></p>'+
-      '<p><b>Fish:</b> Striped Bass</p>'+
-      '<p><b>Tide:</b> Outgoing</p>'+
-      '<p><b>Bait:</b> Clam Bellies</p>'+
-      '<p><b>Length:</b> 30 Inches</p>'+
-      '<p><b>Weight:</b> 25 lbs</p>'+
+      '<p><b>Fish:   </b><select><option>Striper</option><option>Fluke</option></select></p>'+
+      '<p><b>Tide:   </b><select><option>Incoming</option><option>Outgoing</option></select></p>'+
+      '<p><b>Bait:   </b><input id ="txtBait" name="txtBait" class="element text medium<?php if ($firstNameERROR) echo ' mistake'; ?>" type="text" maxlength="100" value="<?php echo $firstName; ?>" onfocus="this.select()"  tabindex="33"/>'+
+      '<p><b>Length: </b><input id ="txtLength" name="txtLength" class="element text medium<?php if ($firstNameERROR) echo ' mistake'; ?>" type="text" maxlength="100" value="<?php echo $firstName; ?>" onfocus="this.select()"  tabindex="33"/> Inches</p>'+
+      '<p><b>Weight: </b><input id ="txtWeight" name="txtWeight" class="element text medium<?php if ($firstNameERROR) echo ' mistake'; ?>" type="text" maxlength="100" value="<?php echo $firstName; ?>" onfocus="this.select()"  tabindex="33"/> Pounds</p>'+
       '</div>'+
       '</div>';
 
